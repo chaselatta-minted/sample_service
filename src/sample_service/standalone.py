37abc162c8c13db8aaf71f44b1a99458572c4468
Chase Latta <chase@minted.com>
2014-10-27 14:46:59 -0700

    Fix broken files

--- src/sample_service/standalone.py
+++ src/sample_service/standalone.py
@@ -2,16 +2,17 @@
 ### ALLOWS THIS TO BE RUN ALONE
 
 
-from m_services.plugin.run import prepare_standalone, run_app
+# from m_services.plugin.run import prepare_standalone, run_app
 from m_services.handlers.handler import BaseHandler
 
-(app, api) = prepare_standalone()
+
+# (app, api) = prepare_standalone()
 
 class Handler(BaseHandler):
   def get(self):
     return self.finalize('x', None)
 
 
-api.add_resource(Handler, '/')
+# api.add_resource(Handler, '/')
 
-run_app(app)
+# run_app(app)
